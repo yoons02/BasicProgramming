3118bca23b4c7cf96238cf4761bf95e6b8bd44f1
yoons02 <kys5037k@naver.com>
2022-05-19 16:58:51 +0900

    task

--- test3.c
+++ test3.c
@@ -5,22 +5,21 @@
 #define b_rowsize 2
 #define b_colsize 3
 
-double sum(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize]);
-double sub(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize]);
-double mul(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize]);
-
+double sum(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize]); // sum fuction declaration
+double sub(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize]); // subraction fuction declaration
+double mul(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize]); // multiple fuction declaration
 int main()
 {
     int i, j;
 
-    double A[a_rowsize][a_colsize], B[b_rowsize][b_colsize], C[a_colsize][b_rowsize];
+    double A[a_rowsize][a_colsize], B[b_rowsize][b_colsize], C[a_colsize][b_rowsize]; // double type matrix
 
     for (i = 0; i < a_rowsize; i++)
     {
         for (j = 0; j < a_colsize; j++)
         {
-            printf("Enter the number of Matrix A[%d][%d] : ", i, j);
-            scanf(" %lf", &A[i][j]);
+            printf("Enter the value of Matrix A[%d][%d] : ", i, j);
+            scanf(" %lf", &A[i][j]); // 'lf' type, input value
         }
     }
     printf("\nMat A\n");
@@ -29,7 +28,7 @@ int main()
     {
         for (j = 0; j < a_colsize; j++)
         {
-            printf("%f ", A[i][j]);
+            printf("%f ", A[i][j]); // output value
         }
         printf("\n");
     }
@@ -40,7 +39,7 @@ int main()
         for (j = 0; j < b_colsize; j++)
         {
             printf("Enter the number of Matrix B[%d][%d] : ", i, j);
-            scanf(" %lf", &B[i][j]);
+            scanf(" %lf", &B[i][j]); // 'lf' type, input value
         }
     }
 
@@ -49,31 +48,31 @@ int main()
     {
         for (j = 0; j < b_colsize; j++)
         {
-            printf("%f ", B[i][j]);
+            printf("%f ", B[i][j]); // output value
         }
         printf("\n");
     }
 
 
     printf("\n");
-    printf("%f\n\n", sum(A, B));
-    printf("%f\n\n", sub(A, B));
-    printf("%f\n\n", mul(A, B));
+    printf("%f\n\n", sum(A, B)); // call sum function
+    printf("%f\n\n", sub(A, B)); // call sub function
+    printf("%f\n\n", mul(A, B)); // call mul function
 }
 
-double sum(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize])
+double sum(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize]) // parameter
 {
     int i, j;
     double C[a_rowsize][a_colsize];
 
     printf("Mat A + Mat B\n");
-    if ((a_rowsize == b_rowsize) && (a_colsize == b_colsize))
+    if ((a_rowsize == b_rowsize) && (a_colsize == b_colsize)) // check sum define
     {
         for (i = 0; i < a_rowsize; i++)
         {
             for (j = 0; j < a_colsize; j++)
             {
-                C[i][j] = A[i][j] + B[i][j];
+                C[i][j] = A[i][j] + B[i][j]; // sum by position
                 printf("%f ", C[i][j]);
             }
             printf("\n");
@@ -87,19 +86,19 @@ double sum(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize])
     }
 }
 
-double sub(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize])
+double sub(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize]) // parameter
 {
     int i, j;
     double C[a_rowsize][a_colsize];
 
     printf("Mat A - Mat B\n");
-    if ((a_rowsize == b_rowsize) && (a_colsize == b_colsize))
+    if ((a_rowsize == b_rowsize) && (a_colsize == b_colsize)) // check subtraction define
     {
         for (i = 0; i < a_rowsize; i++)
         {
             for (j = 0; j < a_colsize; j++)
             {
-                C[i][j] = A[i][j] - B[i][j];
+                C[i][j] = A[i][j] - B[i][j]; // subtration by position
                 printf("%f ", C[i][j]);
             }
             printf("\n");
@@ -113,7 +112,7 @@ double sub(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize])
     }
 }
 
-double mul(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize])
+double mul(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize]) // parameter
 {
     int i, j, k;
     double temp;
@@ -126,10 +125,10 @@ double mul(double A[a_rowsize][a_colsize], double B[b_rowsize][b_colsize])
         {
             for (j = 0; j < b_colsize; j++)
             {
-                temp =0;
+                temp = 0; // initialization
                 for (k = 0; k < a_colsize; k++)
                 {
-                    temp+=A[i][k]*B[k][j];
+                    temp+=A[i][k]*B[k][j]; // one element of Mat C
                     C[i][j]=temp;
                 }
                 printf("%f ", C[i][j]);
---
+++ test4.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+#define a_size 5 // define size
+#define b_size 5
+
+int swap(int *ptrA, int *ptrB); // function declaration
+
+int main()
+{
+    int i, j;
+    int A[a_size], B[b_size];
+
+    if (a_size == b_size)
+    {
+        for (i = 0; i < a_size; i++)
+        {
+            printf("Enter the element of Mat A[%d] : ", i);
+            scanf(" %d", &A[i]); // input Mat A element 
+        }
+        printf("\n");
+
+        for (j = 0; j < b_size; j++)
+        {
+            printf("Enter the element of Mat B[%d] : ", j);
+            scanf(" %d", &B[j]); // input Mat B element 
+        }
+        printf("\n");
+
+        printf("Before swap Mat A, Mat B\n");
+        printf("A = [ ");
+
+        for (i = 0; i < a_size; i++)
+        {
+            printf("%d ", A[i]);
+        }
+
+        printf("]\n");
+
+        printf("B = [ ");
+
+        for (i = 0; i < a_size; i++)
+        {
+            printf("%d ", B[i]);
+        }
+
+        printf("]\n\n");
+
+        printf("%d", swap(A, B)); // swap function call
+    }
+    else
+    {
+        printf("Matrix sizes are not fit\n");
+    }
+} 
+
+int swap(int *ptrA, int *ptrB)
+{
+    int i, j;
+    int temp;
+
+    for(i = 0; i < a_size; i++)
+    {
+        temp= *(ptrA + i);
+        *(ptrA + i) = *(ptrB + i);
+        *(ptrB + i) = temp;
+    }
+
+    printf("After swap Mat A, Mat B\n");
+    printf("A = [ ");
+
+    for (i = 0; i < a_size; i++)
+    {
+        printf("%d ", *(ptrA + i));
+    }
+
+    printf("]\n");
+
+    printf("B = [ ");
+
+    for (i = 0; i < a_size; i++)
+    {
+        printf("%d ", *(ptrB + i));
+    }
+
+    printf("]\n");
+}
