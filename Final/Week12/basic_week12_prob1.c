082ecd5551d06f3a049c692c284770fede82708c
yoons02 <kys5037k@naver.com>
2022-05-27 22:54:38 +0900

    task

--- Final/Week12/basic_week12_prob1.c
+++ Final/Week12/basic_week12_prob1.c
@@ -2,7 +2,7 @@
 # include <string.h>
 
 
-struct Student
+struct Student // struct type
 {
     char name[20];
     char number[20];
@@ -13,50 +13,52 @@ struct Student
 
 int main()
 {
-    struct Student person[1000];
+    struct Student person[1000]; // 1000 students are maximum to save
     int i, j, tot, select, count, common;
 
     printf("Welcome! You can enter data for 1000 student\n");
 
-    loop:
+    loop: // select action
     {   
-        printf("\nPlease enter the number to active(1 : add, 2 : search, 3 : delete, 4 : entire inqury) : ");
+        printf("\nPlease enter the number to active\n");
+        printf("(1 : add, 2 : search, 3 : delete, 4 : entire inqury, 5 : Quit)\n: ");
         scanf("%d", &select);
 
-        switch (select)
+        switch (select) // switch ~ case statement
         {
             case 1:
-                goto new;
+                goto new; // enter the new data of students
                 break;
 
             case 2:
-                goto search;
+                goto search; // find students for searching name
                 break;
 
             case 3:
-                goto delete;
+                goto delete; // delete student data 
                 break;
 
             case 4:
-                goto entire;
+                goto entire; // inqury entire students data
                 break;
 
             case 5:
-                printf("Quit the program\n");
+                goto exit; // program off
                 break;
 
             default:
-                printf("Error : There is no choice\n");
+                printf("Error : There is no choice. Enter number again.\n"); // error
+                goto loop;
                 break;
         }
     }
-    // 신규입력
-    new:
+    
+    new: // new loop
     {
         printf("How many students do you want to enter? : ");
         scanf("%d", &count);
 
-        for (tot = j; tot < j + count; tot++)
+        for (tot = j; tot < j + count; tot++) 
         {
             printf("\nname[%d] : ", tot);
             scanf("%s", person[tot].name);
@@ -73,69 +75,64 @@ int main()
             printf("address[%d] : ", tot);
             scanf("%s", person[tot].address);
         }
-        j = tot - 1;
+        j = tot - 1; // save data at next array
         printf("\nSave complete\n");
 
         goto loop;
     }
 
-    //이름 검색
-    search:
+    search: // searching name loop
     {
-        struct Student *p = NULL;   // 구조체의 포인터 변수 p를 생성한다. 현재 아무 주소도 가리키지 않고 있다.
-
-        int size = sizeof(person) / sizeof(person[0]);   // 구조체의 변수 pb[]에 대한 index 개수를 구한다.
-        char in_name[20];                     // 입력받을 이름을 저장할 변수이다.
+        struct Student *p = NULL; // pointer p
+        char in_name[20];  // save input name data
 
         printf("\nEnter the name for searching : ");
         scanf("%s", &in_name);
 
-        common = 0;
+        common = 0; // count common name
         for(int i = 0; i < tot; i++)
-        {          // pb[] 배열을 size(index의 개수) 만큼 반복하며 찾는다.
-            if(strcmp(in_name, person[i].name) == 0)
-            {   // name과 pb[i].name의 문자열이 같으면 0을 반환한다.
+        {
+            if(strcmp(in_name, person[i].name) == 0) // if in_name and person[i].name are same
+            {
                 p = &person[i];
                 common++; 
                 
                 printf("\n%s[%d]'s information", in_name, i);
-                printf("\nname : %s", p->name);
-                printf("\nnumber : %s", p->number);
-                printf("\nmajor : %s", p->major);
-                printf("\nage : %s", p->age);
-                printf("\naddress : %s\n", p->address);// 구조체의 포인터 변수는 '->'를 이용하여 멤버변수에 접근한다.
-                // 그리고 name과 pb[i].name이 같으면 구조체의 포인터 변수 *p에 해당하는 pb[] index의 주소값을 저장한다.
+                printf("\nname : %s", p->name); // access name memory
+                printf("\nnumber : %s", p->number); // access number memory
+                printf("\nmajor : %s", p->major); // access major memory
+                printf("\nage : %s", p->age); // access age memory
+                printf("\naddress : %s\n", p->address); // access address memory
             } 
-        }                                // 검색할 이름을 찾았기 때문에 for문을 빠져 나온다.
+        }
         
-        printf("\nTheir are %d informations\n", common);
+        printf("\nTheir are %d informations\n", common); // print the number of same name
         goto loop;
     }
 
-    //삭제 기능
-    delete:
+    delete: // delete loop
     {
         char find[20];
 
-        printf("\nEnter the number for delete : ");
+        printf("\nEnter the number for delete : "); 
         scanf("%s", &find);
 
         for (i = 0; i < tot; i++)
         {
-            if (strcmp(find, person[i].name) == 0)
+            if (strcmp(find, person[i].name) == 0) // if find and person[i].name are same
             {
-                person[i] = person[i+1]; 
+                person[i] = person[i+1]; // overwrite following data
             }
+            person[i+1] = person[i+2]; // overwrith following data others too
         }
-        tot = tot - 1; 
-
+        tot = tot - 1; // total number of student is decrease (-1)
         printf("\nDelete complete\n");
+
         goto loop;
     }
 
-    entire:
+    entire: // inqury all loop
     {
-        
         for (i = 0; i < tot; i++)
         {   
             printf("\nstudent[%d]", i);
@@ -149,6 +146,11 @@ int main()
         printf("\nDon't have any more information\n");
 
         goto loop;
+    }
+
+    exit:
+    {
+        printf("Quit the program\n");
     }
 
     return 0;
---
+++ test_1/test7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5]={0};
+
+    arr[1] = 2;
+    printf("%d", arr[2]);
+}
